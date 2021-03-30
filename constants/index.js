c04e8af30a2336f7663058f78b1c6906c7a710bd
sanimridha <sanimridha307@gmail.com>
2021-03-30 15:11:27 +0600

    exported all constants from index

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import icons from './icons';
+import images from './images';
+import GOOGLE_API_KEY from './maps';
+import {COLORS, FONTS, SIZES} from './theme';
+
+export {images, icons, GOOGLE_API_KEY, COLORS, FONTS, SIZES};
