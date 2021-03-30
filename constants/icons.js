23fe5d0cb7f9860c8fb8df03a9736205390809b7
sanimridha <sanimridha307@gmail.com>
2021-03-30 12:38:18 +0600

    created icon refrance

---
+++ constants/icons.js
@@ -0,0 +1,23 @@
+export const Home = require('../assets/icons/Home.png');
+export const About = require('../assets/icons/About.png');
+export const Contact = require('../assets/icons/Contact.png');
+export const GoBack = require('../assets/icons/GoBack.png');
+export const GoForward = require('../assets/icons/GoForward.png');
+export const LeftArrow = require('../assets/icons/LeftArrow.png');
+export const RightArrow = require('../assets/icons/RightArrow.png');
+export const Location = require('../assets/icons/Location.png');
+export const Projects = require('../assets/icons/Projects.png');
+export const Services = require('../assets/icons/Services.png');
+
+export default {
+  Home,
+  About,
+  Contact,
+  GoBack,
+  GoForward,
+  LeftArrow,
+  RightArrow,
+  Location,
+  Projects,
+  Services,
+};
