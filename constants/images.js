01482ee22bfdb2349082398cce753653fa00f073
sanimridha <sanimridha307@gmail.com>
2021-03-30 13:18:45 +0600

    created image refrance

---
+++ constants/images.js
@@ -0,0 +1,65 @@
+const about_1 = require('../assets/images/about/1.jpg');
+const about_2 = require('../assets/images/about/2.jpg');
+const about_3 = require('../assets/images/about/3.jpg');
+const about_4 = require('../assets/images/about/4.jpg');
+const home = require('../assets/images/home/home.png');
+const logo = require('../assets/images/logo/Logo.png');
+const splash = require('../assets/images/logo/Splash.png');
+const onboarding_1 = require('../assets/images/onboarding/1.png');
+const onboarding_2 = require('../assets/images/onboarding/2.png');
+const onboarding_3 = require('../assets/images/onboarding/3.png');
+const s_backend = require('../assets/images/servicesimage/backend.png');
+const s_frontend = require('../assets/images/servicesimage/frontend.png');
+const s_pwa = require('../assets/images/servicesimage/pwa.png');
+const s_spa = require('../assets/images/servicesimage/spa.png');
+const Algolia = require('../assets/images/technology/Backendimages/Algolia.png');
+const Elasticsearch = require('../assets/images/technology/Backendimages/Elasticsearch.png');
+const firebase = require('../assets/images/technology/Backendimages/firebase.png');
+const laravel = require('../assets/images/technology/Backendimages/laravel.png');
+const MySQL = require('../assets/images/technology/Backendimages/MySQL.png');
+const php = require('../assets/images/technology/Backendimages/php.png');
+const Redis = require('../assets/images/technology/Backendimages/Redis.png');
+const css = require('../assets/images/technology/frontendimages/css.png');
+const html = require('../assets/images/technology/frontendimages/html.png');
+const react = require('../assets/images/technology/frontendimages/react.png');
+const Tailwind = require('../assets/images/technology/frontendimages/Tailwind.png');
+const ts = require('../assets/images/technology/frontendimages/ts.png');
+const vue = require('../assets/images/technology/frontendimages/vue.png');
+const ai = require('../assets/images/technology/UIUX/ai.png');
+const xd = require('../assets/images/technology/UIUX/xd.png');
+const figma = require('../assets/images/technology/UIUX/figma.png');
+const ps = require('../assets/images/technology/UIUX/ps.png');
+
+export default {
+  about_1,
+  about_2,
+  about_3,
+  about_4,
+  home,
+  logo,
+  splash,
+  onboarding_1,
+  onboarding_2,
+  onboarding_3,
+  s_backend,
+  s_frontend,
+  s_pwa,
+  s_spa,
+  Algolia,
+  Elasticsearch,
+  firebase,
+  laravel,
+  MySQL,
+  php,
+  Redis,
+  css,
+  html,
+  react,
+  Tailwind,
+  ts,
+  vue,
+  ai,
+  xd,
+  figma,
+  ps,
+};
