16bea0c775e10923036bcc8d6cbd2f5d0f8917a0
sanimridha <sanimridha307@gmail.com>
2021-03-30 17:25:08 +0600

    Creating Screens Refrance

---
+++ screens/index.js
@@ -0,0 +1,19 @@
+import About from './About';
+import Career from './Career';
+import CareerDetails from './CareerDetails';
+import Contact from './Contact';
+import Home from './Home';
+import ProjectDetails from './ProjectDetails';
+import Projects from './Projects';
+import Services from './Services';
+
+export {
+  About,
+  Career,
+  CareerDetails,
+  Contact,
+  Home,
+  ProjectDetails,
+  Projects,
+  Services,
+};
