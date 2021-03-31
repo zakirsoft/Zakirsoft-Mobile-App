f7403fa4bc33c89e45164b936fa040f59b18dc5a
sanimridha <sanimridha307@gmail.com>
2021-03-31 14:19:22 +0600

    impplemented Home Screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,86 @@
 import React from 'react';
-import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {COLORS, images, SIZES, FONTS} from '../constants';
-import {Screen, Logo} from '../Styles/Screen';
+import {Screen, Logo, PrimaryButton, SecondaryButton} from '../Styles/Screen';
 
 const Home = () => {
   return (
     <Screen>
-      <Logo>
-        <Image source={images.logo} style={{height: 24, width: 120}} />
-      </Logo>
+      <View style={{flexDirection: 'column'}}>
+        <View>
+          <Image
+            source={images.home}
+            resizeMode="contain"
+            style={{height: 370, width: 369, top: -10}}
+          />
+        </View>
+        <View style={{flexDirection: 'column', top: -55}}>
+          <Text
+            style={{
+              fontSize: SIZES.largeTitle,
+              fontWeight: 'bold',
+            }}>
+            We are here to
+          </Text>
+          <Text
+            style={{
+              fontSize: SIZES.largeTitle,
+              fontWeight: 'bold',
+            }}>
+            grow your
+          </Text>
+          <Text
+            style={{
+              fontSize: SIZES.largeTitle,
+              fontWeight: 'bold',
+            }}>
+            business.
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'column',
+            top: -55,
+            // paddingLeft: 25,
+            paddingRight: 24,
+          }}>
+          <Text style={{fontSize: SIZES.body3, color: COLORS.secondary}}>
+            As a software company we develop innovatives website & software
+            solutions. We are proud of what we do.
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            top: -25,
+          }}>
+          <PrimaryButton style={{marginRight: 8}}>
+            <Text
+              style={{
+                fontSize: SIZES.body3,
+                fontWeight: 'bold',
+                color: COLORS.white,
+              }}>
+              Contact Us
+            </Text>
+          </PrimaryButton>
+          <SecondaryButton
+            style={{
+              borderColor: COLORS.lightGray,
+              borderWidth: 1,
+              borderRadius: 4,
+            }}>
+            <Text
+              style={{
+                fontSize: SIZES.body3,
+                fontWeight: 'bold',
+                color: COLORS.black,
+              }}>
+              View Project
+            </Text>
+          </SecondaryButton>
+        </View>
+      </View>
     </Screen>
   );
 };
