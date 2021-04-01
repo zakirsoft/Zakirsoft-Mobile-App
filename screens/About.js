f95ae4be8c09ba86c9cbbaa9266dff71f561290f
sanimridha <sanimridha307@gmail.com>
2021-04-01 17:17:28 +0600

    dummy data

--- screens/About.js
+++ screens/About.js
@@ -1,10 +1,65 @@
 import React from 'react';
-import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  Animated,
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {COLORS, images, SIZES, FONTS} from '../constants';
 import {Screen, Logo} from '../Styles/Screen';
 
+const AboutImgData = [
+  {
+    id: '1',
+    Img: Image.about_1,
+  },
+  {
+    id: '2',
+    Img: Image.about_2,
+  },
+  {id: '3', Img: Image.about_3},
+  {
+    id: '4',
+    Img: Image.about_4,
+  },
+];
+
 const About = () => {
-  return <Screen></Screen>;
+  const RenderImageList = () => {
+    // <Animated.ScrollView
+    //   horizontal
+    //   pagingEnabled
+    //   scrollEventThrottle={16}
+    //   snapToAlignment="center"
+    //   showsHorizontalScrollIndicator={false}
+    //   // onScroll
+    // ></Animated.ScrollView>;
+    // return (
+    //   <View>
+    //     <FlatList
+    //       data={AboutImgData}
+    //       keyExtractor={item => item.id}
+    //       horizontal
+    //       scrollEnabled
+    //       snapToAlignment="center"
+    //       scrollEventThrottle={16}
+    //       showsHorizontalScrollIndicator={false}
+    //       renderItem={({item}) => (
+    //         <Image
+    //           source={item.Img}
+    //           style={{height: 200, width: 200}}
+    //           resizeMode="contain"
+    //         />
+    //       )}
+    //     />
+    //   </View>
+    // );
+  };
+
+  return <Screen>{RenderImageList()}</Screen>;
 };
 
 export default About;
