0bfc55bad645af1f4067c4cc80950c9afb5847be
sanimridha <sanimridha307@gmail.com>
2021-04-05 12:01:33 +0600

    contact screen complete

--- screens/Contact.js
+++ screens/Contact.js
@@ -8,7 +8,7 @@ import {
   View,
 } from 'react-native';
 import {COLORS, images, SIZES, FONTS, icons} from '../constants';
-import {Screen, Logo} from '../Styles/Screen';
+import {Screen, Logo, PrimaryButton} from '../Styles/Screen';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 const Contact = () => {
@@ -41,68 +41,189 @@ const Contact = () => {
     </View>
   );
   const renderInfo = () => (
-    <View
-      style={{
-        width: 366,
-        height: 240,
-        borderRadius: 10,
-        top: -30,
-        backgroundColor: COLORS.white,
-        marginLeft: 24,
-        position: 'relative',
-      }}>
-      <View style={{flexDirection: 'row', marginLeft: 16, marginTop: 16}}>
+    <>
+      <View
+        style={{
+          width: 366,
+          height: 240,
+          borderRadius: 10,
+          top: -40,
+          backgroundColor: COLORS.white,
+          marginLeft: 24,
+          position: 'relative',
+        }}>
         <View
           style={{
-            height: 48,
-            width: 48,
-            borderRadius: 10,
-            backgroundColor: COLORS.ScreenColor,
-            justifyContent: 'center',
-            alignItems: 'center',
+            flexDirection: 'row',
+            marginLeft: 16,
+            marginTop: 16,
+            marginBottom: 16,
           }}>
-          <Image
-            source={icons.Location}
-            resizeMode={'contain'}
+          <View
             style={{
-              height: 22,
-              width: 18,
-              alignItems: 'center',
+              height: 48,
+              width: 48,
+              borderRadius: 10,
+              backgroundColor: COLORS.ScreenColor,
               justifyContent: 'center',
-            }}
-          />
+              alignItems: 'center',
+            }}>
+            <Image
+              source={icons.Location}
+              resizeMode={'contain'}
+              style={{
+                height: 22,
+                width: 18,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            />
+          </View>
+          <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            <Text
+              style={{
+                fontFamily: 'DMSans-Regular',
+                fontSize: 16,
+                color: COLORS.black,
+                marginLeft: 10,
+              }}>
+              Home# 1024/N, Road# 17/A, Adabor, Dhaka-1207
+            </Text>
+          </View>
         </View>
-        <Text
+        <View
           style={{
-            fontFamily: 'DMSans-Regular',
-            fontSize: 16,
-            color: COLORS.black,
-            marginLeft: 10,
+            width: 330,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: COLORS.ScreenColor,
+            // top: 80,
+            height: 1,
+            left: 16,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'row',
+            marginLeft: 16,
+            marginTop: 16,
+            marginBottom: 16,
           }}>
-          Home# 1024/N, Road# 17/A, Adabor, Dhaka-1207
-        </Text>
+          <View
+            style={{
+              height: 48,
+              width: 48,
+              borderRadius: 10,
+              backgroundColor: COLORS.ScreenColor,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Image
+              source={icons.Call}
+              resizeMode={'contain'}
+              style={{
+                height: 22,
+                width: 18,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            />
+          </View>
+          <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            <Text
+              style={{
+                fontFamily: 'DMSans-Regular',
+                fontSize: 16,
+                color: COLORS.black,
+                marginLeft: 10,
+              }}>
+              +8801625 592566
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            width: 330,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: COLORS.ScreenColor,
+            // top: 80,
+            height: 1,
+            left: 16,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'row',
+            marginLeft: 16,
+            marginTop: 16,
+            marginBottom: 16,
+          }}>
+          <View
+            style={{
+              height: 48,
+              width: 48,
+              borderRadius: 10,
+              backgroundColor: COLORS.ScreenColor,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Image
+              source={icons.Call}
+              resizeMode={'contain'}
+              style={{
+                height: 22,
+                width: 18,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            />
+          </View>
+          <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            <Text
+              style={{
+                fontFamily: 'DMSans-Regular',
+                fontSize: 16,
+                color: COLORS.black,
+                marginLeft: 10,
+              }}>
+              zakirsoft20@gmail.com
+            </Text>
+          </View>
+        </View>
       </View>
       <View
         style={{
-          width: 330,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: COLORS.lightGray,
-          // top: 80,
-          height: 1,
-          left: 10,
-        }}></View>
-      <View
-        style={{
-          width: 330,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: COLORS.lightGray,
-          top: 80,
-          height: 1,
-          left: 10,
-        }}></View>
-    </View>
+          width: 366,
+          height: 240,
+          borderRadius: 10,
+          top: -40,
+          marginLeft: 24,
+          position: 'relative',
+          marginTop: 16,
+        }}>
+        <PrimaryButton style={{width: 366}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: 'bold',
+                color: COLORS.white,
+              }}>
+              Call Now
+            </Text>
+            <Image
+              source={icons.Call}
+              resizeMode={'contain'}
+              style={{height: 20, width: 20, tintColor: COLORS.white}}
+            />
+          </View>
+        </PrimaryButton>
+      </View>
+    </>
   );
 
   LogBox.ignoreAllLogs();
