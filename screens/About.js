5a1aaf2d1eb985030f8669e5e40d80916cb7bb45
sanimridha <sanimridha307@gmail.com>
2021-04-07 18:55:09 +0600

    horizontal scrollable images

--- screens/About.js
+++ screens/About.js
@@ -14,52 +14,41 @@ import {Screen, Logo} from '../Styles/Screen';
 const AboutImgData = [
   {
     id: '1',
-    Img: Image.about_1,
+    Img: images.about_1,
   },
   {
     id: '2',
-    Img: Image.about_2,
+    Img: images.about_2,
   },
-  {id: '3', Img: Image.about_3},
+  {id: '3', Img: images.about_3},
   {
     id: '4',
-    Img: Image.about_4,
+    Img: images.about_4,
   },
 ];
 
 const About = () => {
-  const RenderImageList = () => {
-    // <Animated.ScrollView
-    //   horizontal
-    //   pagingEnabled
-    //   scrollEventThrottle={16}
-    //   snapToAlignment="center"
-    //   showsHorizontalScrollIndicator={false}
-    //   // onScroll
-    // ></Animated.ScrollView>;
-    // return (
-    //   <View>
-    //     <FlatList
-    //       data={AboutImgData}
-    //       keyExtractor={item => item.id}
-    //       horizontal
-    //       scrollEnabled
-    //       snapToAlignment="center"
-    //       scrollEventThrottle={16}
-    //       showsHorizontalScrollIndicator={false}
-    //       renderItem={({item}) => (
-    //         <Image
-    //           source={item.Img}
-    //           style={{height: 200, width: 200}}
-    //           resizeMode="contain"
-    //         />
-    //       )}
-    //     />
-    //   </View>
-    // );
-  };
+  const renderItem = ({item}) => (
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Image source={item.Img} style={{height: 250}} />
+      <Text>{item.id}</Text>
+    </View>
+  );
 
-  return <Screen>{RenderImageList()}</Screen>;
+  return (
+    <Screen>
+      <FlatList
+        data={AboutImgData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+        snapToAlignment="center"
+      />
+    </Screen>
+  );
 };
 
 export default About;
