999ac29825c3b4859b811900cec936f5c070add1
sanimridha <sanimridha307@gmail.com>
2021-04-20 03:19:19 +0600

    added flartlist inside flatlist

--- screens/NewsDetails.js
+++ screens/NewsDetails.js
@@ -1,9 +1,228 @@
-import React from 'react';
-import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
-import {COLORS, images, SIZES} from '../constants';
-import {PrimaryButton} from '../Styles/Screen';
+import React, {useState} from 'react';
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {COLORS, icons, images, SIZES} from '../constants';
+import {PrimaryButton, TechCart} from '../Styles/Screen';
 
-const NewsDetails = ({route}) => {
+const NewsDetails = ({route, navigation}) => {
+  const renderNews = () => {
+    const NewsData = [
+      {
+        id: 1,
+        image: images.News_1,
+        title: 'We Just Redesign Our Website',
+        description:
+          'Pellentesque sagittis, quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi.',
+        longDescription:
+          'Pellentesque sagittis, quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi. Pellentesque sagittis,\n\n quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi Pellentesque sagittis, quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi',
+        link: 'https://zakirsoft.com/',
+      },
+      {
+        id: 2,
+        image: images.News_2,
+        title: 'Zakirsoft hired new Designer',
+        description:
+          'Pellentesque sagittis, quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi.',
+        longDescription:
+          'Pellentesque sagittis, quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi. Pellentesque sagittis,\n\n quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi Pellentesque sagittis, quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi',
+        link: 'https://zakirsoft.com/',
+      },
+      {
+        id: 3,
+        image: images.News_3,
+        title: 'We announced 100M invesment',
+        description:
+          'Pellentesque sagittis, quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi.',
+        longDescription:
+          'Pellentesque sagittis, quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi. Pellentesque sagittis,\n\n quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi Pellentesque sagittis, quam vel tincidunt ullamcorper, massa purus egestas libero, nec porttitor augue leo sed mi',
+        link: 'https://zakirsoft.com/',
+      },
+    ];
+    const [active, setActive] = useState(0);
+    const change = ({nativeEvent}) => {
+      const slide = Math.ceil(
+        nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width,
+      );
+      if (slide !== active) {
+        setActive(slide);
+      }
+    };
+    const renderItem = ({item}) => {
+      return (
+        <TechCart
+          style={{
+            height: 450,
+            width: 350,
+            marginBottom: 20,
+          }}>
+          {/* <View style={{justifyContent: 'center'}}> */}
+          <Image
+            source={item.image}
+            resizeMode={'cover'}
+            style={{
+              height: 260,
+              width: '100%',
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+            }}
+          />
+          {/* </View> */}
+          <View style={{marginTop: 24, marginLeft: 16}}>
+            <Text
+              style={{
+                fontSize: 20,
+                fontFamily: 'DMSans-Regular',
+                fontWeight: 'bold',
+              }}>
+              {item.title}
+            </Text>
+            <Text
+              style={{
+                fontSize: SIZES.body3,
+                color: COLORS.secondary,
+                lineHeight: 24,
+                paddingTop: 8,
+              }}>
+              {item.description}
+            </Text>
+            <TouchableOpacity
+              style={{marginTop: 20}}
+              onPress={() => {
+                navigation.navigate('NewsDetails', item);
+              }}>
+              <View style={{flexDirection: 'row'}}>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    fontFamily: 'DMSans-Regular',
+                    fontWeight: 'bold',
+                    color: COLORS.primary,
+                  }}>
+                  Read More
+                </Text>
+                <View
+                  style={{
+                    justifyContent: 'center',
+                    // alignItems: 'center',
+                    top: 1,
+                    marginLeft: 8,
+                  }}>
+                  <Image
+                    source={icons.RightArrow}
+                    resizeMode={'contain'}
+                    style={{
+                      width: 20,
+                      height: 11,
+                      tintColor: COLORS.primary,
+                    }}
+                  />
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </TechCart>
+      );
+    };
+
+    return (
+      <View style={{}}>
+        <View
+          style={{
+            marginTop: 52,
+            flexDirection: 'row',
+            position: 'relative',
+          }}>
+          <Text
+            style={{
+              fontSize: 28,
+              fontFamily: 'DMSans-Regular',
+              fontWeight: 'bold',
+            }}>
+            Our Latest News
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'stretch',
+              justifyContent: 'space-between',
+              marginLeft: 56,
+            }}>
+            <TouchableOpacity>
+              <View
+                style={{
+                  height: 36,
+                  width: 36,
+                  borderRadius: 18,
+                  backgroundColor: COLORS.primary,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Image
+                  source={icons.GoBack}
+                  style={{tintColor: COLORS.white, height: 16, width: 16}}
+                  resizeMode={'contain'}
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View
+                style={{
+                  height: 36,
+                  width: 36,
+                  borderRadius: 18,
+                  backgroundColor: COLORS.white,
+                  marginLeft: 5,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Image
+                  source={icons.GoForward}
+                  style={{tintColor: COLORS.black, height: 16, width: 16}}
+                  resizeMode={'contain'}
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View>
+          <FlatList
+            data={NewsData}
+            renderItem={renderItem}
+            keyExtractor={item => `${item.id}`}
+            horizontal
+            onScroll={change}
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled={true}
+          />
+          <View
+            style={{
+              flexDirection: 'row',
+              // position: 'relative',
+              marginBottom: 50,
+              alignSelf: 'center',
+            }}>
+            {NewsData.map((i, k) => (
+              <Text
+                key={k}
+                style={{
+                  color: k == active ? COLORS.primary : '#C4D6FB',
+                  marginRight: 8,
+                }}>
+                ⬤
+              </Text>
+            ))}
+          </View>
+        </View>
+      </View>
+    );
+  };
   const {image, title, longDescription} = route.params;
   return (
     <ScrollView style={{backgroundColor: COLORS.ScreenColor}}>
@@ -45,7 +264,9 @@ const NewsDetails = ({route}) => {
               Contact
             </Text>
           </PrimaryButton>
+          <View></View>
         </View>
+        {renderNews()}
       </View>
     </ScrollView>
   );
