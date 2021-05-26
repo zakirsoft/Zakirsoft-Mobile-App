b766260de6997f5717b3777c67dbe4c810461992
sanimridha <sanimridha307@gmail.com>
2021-05-26 15:52:03 +0600

    import all tech images to images.js constants

--- constants/images.js
+++ constants/images.js
@@ -12,23 +12,56 @@ const s_backend = require('../assets/images/servicesimage/backend.png');
 const s_frontend = require('../assets/images/servicesimage/frontend.png');
 const s_pwa = require('../assets/images/servicesimage/pwa.png');
 const s_spa = require('../assets/images/servicesimage/spa.png');
-// const Algolia = require('../assets/images/technology/Backendimages/Algolia.png');
-// const Elasticsearch = require('../assets/images/technology/Backendimages/Elasticsearch.png');
-// const firebase = require('../assets/images/technology/Backendimages/firebase.png');
-// const laravel = require('../assets/images/technology/Backendimages/laravel.png');
-// const MySQL = require('../assets/images/technology/Backendimages/MySQL.png');
-// const php = require('../assets/images/technology/Backendimages/php.png');
-// const Redis = require('../assets/images/technology/Backendimages/Redis.png');
-// const css = require('../assets/images/technology/frontendimages/css.png');
-// const html = require('../assets/images/technology/frontendimages/html.png');
-// const react = require('../assets/images/technology/frontendimages/react.png');
-// const Tailwind = require('../assets/images/technology/frontendimages/Tailwind.png');
-// const ts = require('../assets/images/technology/frontendimages/ts.png');
-// const vue = require('../assets/images/technology/frontendimages/vue.png');
-// const ai = require('../assets/images/technology/UIUX/ai.png');
-// const xd = require('../assets/images/technology/UIUX/xd.png');
-// const figma = require('../assets/images/technology/UIUX/figma.png');
-// const ps = require('../assets/images/technology/UIUX/ps.png');
+// backend images
+const Algolia = require('../assets/images/technology/SVG/Back/Algolia.png');
+const Elasticsearch = require('../assets/images/technology/SVG/Back/Elasticsearch.png');
+const laravel = require('../assets/images/technology/SVG/Back/laravel.png');
+const Liveware = require('../assets/images/technology/SVG/Back/Liveware.png');
+const mysql = require('../assets/images/technology/SVG/Back/mysql-5.png');
+const php = require('../assets/images/technology/SVG/Back/php.png');
+const PHPUnit = require('../assets/images/technology/SVG/Back/PHPUnit-Logo.wine.png');
+const redis = require('../assets/images/technology/SVG/Back/redis.png');
+const RestAPI = require('../assets/images/technology/SVG/Back/RestAPI.png');
+// frontend images
+const Bootstrap = require('../assets/images/technology/SVG/Front/Bootstrap5.png');
+const CSS3 = require('../assets/images/technology/SVG/Front/CSS3.png');
+const Javascript = require('../assets/images/technology/SVG/Front/Javascript.png');
+const jQuery = require('../assets/images/technology/SVG/Front/jQuery.png');
+const npm = require('../assets/images/technology/SVG/Front/npm.png');
+const Nuxtjs = require('../assets/images/technology/SVG/Front/Nuxtjs.png');
+const sass = require('../assets/images/technology/SVG/Front/sass-1.png');
+const Vuejs = require('../assets/images/technology/SVG/Front/Vuejs.png');
+const webpack = require('../assets/images/technology/SVG/Front/webpack-icon.png');
+const yarn = require('../assets/images/technology/SVG/Front/yarn.png');
+// UI/UX images
+const figma = require('../assets/images/technology/SVG/UI/figma.png');
+const illustrator = require('../assets/images/technology/SVG/UI/illustrator.png');
+const photoshop = require('../assets/images/technology/SVG/UI/photoshop.png');
+const Xd = require('../assets/images/technology/SVG/UI/Xd.png');
+// Mobile images
+const aws = require('../assets/images/technology/SVG/mobi/aws-2.png');
+const expo = require('../assets/images/technology/SVG/mobi/expo-seeklogo.com.png');
+const firebase = require('../assets/images/technology/SVG/mobi/firebase-1.png');
+const GoogleAPIs = require('../assets/images/technology/SVG/mobi/GoogleAPIs.png');
+const react = require('../assets/images/technology/SVG/mobi/react-2.png');
+const ReactNavigation = require('../assets/images/technology/SVG/mobi/ReactNavigation.png');
+// Server images
+const digitalocean = require('../assets/images/technology/SVG/Server/digitalocean.png');
+const docker = require('../assets/images/technology/SVG/Server/docker.png');
+const forge = require('../assets/images/technology/SVG/Server/forge.png');
+const netlify = require('../assets/images/technology/SVG/Server/netlify.png');
+const nginx = require('../assets/images/technology/SVG/Server/nginx.png');
+const ubuntu = require('../assets/images/technology/SVG/Server/ubuntu.png');
+// Others images
+const git = require('../assets/images/technology/SVG/Others/git.png');
+const github = require('../assets/images/technology/SVG/Others/github.png');
+const googleDrive = require('../assets/images/technology/SVG/Others/googleDrive.png');
+const mega = require('../assets/images/technology/SVG/Others/mega.png');
+const notion = require('../assets/images/technology/SVG/Others/notion.png');
+const slack = require('../assets/images/technology/SVG/Others/slack.png');
+const terminal = require('../assets/images/technology/SVG/Others/terminal.png');
+const trello = require('../assets/images/technology/SVG/Others/trello.png');
+
 const loovum = require('../assets/images/Projects/loovum.png');
 const News_1 = require('../assets/images/about/News.png');
 const FreelancerManagementSpa = require('../assets/images/Projects/Screenshot.png');
@@ -56,23 +89,15 @@ export default {
   s_frontend,
   s_pwa,
   s_spa,
-  // Algolia,
-  // Elasticsearch,
-  // firebase,
-  // laravel,
-  // MySQL,
-  // php,
-  // Redis,
-  // css,
-  // html,
-  // react,
-  // Tailwind,
-  // ts,
-  // vue,
-  // ai,
-  // xd,
-  // figma,
-  // ps,
+  Algolia,
+  Elasticsearch,
+  laravel,
+  Liveware,
+  mysql,
+  php,
+  PHPUnit,
+  redis,
+  RestAPI,
   loovum,
   News_1,
   News_2,
@@ -84,4 +109,47 @@ export default {
   codeSikhi_SS,
   EchoTemplate_Logo,
   EchoTemplate_SS,
+  git,
+  github,
+  googleDrive,
+  mega,
+  notion,
+  slack,
+  terminal,
+  trello,
+  digitalocean,
+  docker,
+  forge,
+  netlify,
+  nginx,
+  ubuntu,
+  aws,
+  expo,
+  firebase,
+  GoogleAPIs,
+  react,
+  ReactNavigation,
+  figma,
+  illustrator,
+  photoshop,
+  Xd,
+  Algolia,
+  Elasticsearch,
+  laravel,
+  Liveware,
+  mysql,
+  php,
+  PHPUnit,
+  redis,
+  RestAPI,
+  Bootstrap,
+  CSS3,
+  Javascript,
+  jQuery,
+  npm,
+  Nuxtjs,
+  sass,
+  Vuejs,
+  webpack,
+  yarn,
 };
